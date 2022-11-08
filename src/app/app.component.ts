6bd3c588df268fec2473d5f74aa277edbf8f2fac
HoutarouM <marian.horoshko@gmail.com>
2022-11-08 09:38:03 +0100

    lesson four

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
-import { Firma } from './firma.interface';
+import { Firma, FirmType } from './firma.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './myFile.html',
   styleUrls: ['./style.css'],
 })
-export class AppComponent implements Firma {
-  title: string = 'IT Firma';
+export class AppComponent {
+  firma: Firma = {
+    title: 'IT Firma',
+    logo: 'assets/images/logo.jpg',
+    noLogo: 'assets/images/nofoto.jpg',
+    adres: [
+      "Adres 24",
+      "41-400",
+      "Middle of nowhere"
+    ],
+    type: FirmType.finansy
+  }
 
   color: string = 'white';
 
-  logo: string = 'assets/images/logo.jpg';
-
   active: boolean = true;
+
+  showImage: boolean = false;
 
   changeBackground() {
     this.color = this.color === 'white' ? 'coral' : 'white';
--- src/app/firma.interface.ts
+++ src/app/firma.interface.ts
@@ -1,4 +1,12 @@
 export interface Firma {
   title: string;
   logo: string;
+  noLogo: string;
+  adres: string[];
+  type?: FirmType;
+}
+
+export enum FirmType {
+  finansy = 1,
+  it,
 }
